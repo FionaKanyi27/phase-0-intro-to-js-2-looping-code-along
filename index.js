d60e80bcb953d34ea479635688afd27418d7286a
Fiona Kanyi <fionakanyi0@gmail.com>
2024-10-07 15:46:31 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solutions in this file
+// writeCards function
+function writeCards(names, event) {
+    let messages = [];
+    
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    
+    return messages;
+  }
+  
+  // Example usage of writeCards
+  console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+  
+  
+  // countDown function
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--; // Decrement the number
+    }
+  }
+  
+  // Example usage of countDown
+  countDown(10);
+  
